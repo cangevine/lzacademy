28a521b6a35da9bd03c03bfae50bbce5c377b600
Colin Angevine <colinangevine@gmail.com>
2011-01-18 15:24:56 -0500

    Removed all SMTP settings while trying to get SendGrid to work...

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Lzacademy::Application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.sendgrid.net",
-  :port                 => 25,
-  :domain               => ENV['SENDGRID_DOMAIN'],
-  :user_name            => ENV['SENDGRID_USERNAME'],
-  :password             => ENV['SENDGRID_PASSWORD'],
-  :authentication       => :plain
-}
