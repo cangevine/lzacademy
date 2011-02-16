88bbbb36689375fd3631b4c1998a80d8dfcef8a8
Colin Angevine <colinangevine@gmail.com>
2011-02-16 01:25:15 -0500

    Corrected the db migration... for a third time. Giving up on changing to boolean field.

--- db/migrate/20110118044722_correct_emergency_form_types.rb
+++ db/migrate/20110118044722_correct_emergency_form_types.rb
@@ -4,14 +4,6 @@ class CorrectEmergencyFormTypes < ActiveRecord::Migration
       change_column :emergency_forms, :learning_differences, :text
       change_column :emergency_forms, :allergies, :text
       change_column :emergency_forms, :food_restrictions, :text
-      
-      add_column :emergency_forms, :advertisement_permission_tmp, :boolean
-      EmergencyForm.all.each do |ef|
-        ef.advertisement_permission_tmp = advertisement_permission == 'true'
-        ef.save
-      end
-      remove_column :emergency_forms, :advertisement_permission
-      rename_column :emergency_forms, :advertisement_permission_tmp, :advertisement_permission
     end
   end
 
@@ -20,7 +12,6 @@ class CorrectEmergencyFormTypes < ActiveRecord::Migration
       change_column :emergency_forms, :learning_differences, :string
       change_column :emergency_forms, :allergies, :string
       change_column :emergency_forms, :food_restrictions, :string
-      change_column :emergency_forms, :advertisement_permission, :string
     end
   end
 end
