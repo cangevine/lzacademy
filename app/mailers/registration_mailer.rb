a6ccedceb52e89592abfbe9601c855493e39b32d
Colin Angevine <colinangevine@gmail.com>
2011-02-17 01:29:31 -0500

    Corrected the registration mailer to send admin emails

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,16 +1,15 @@
 class RegistrationMailer < ActionMailer::Base
   default :from => "LinguaZone Academy <info@lzacademy.com>"
+  default :bcc => ["info@lzacademy.com", "magistraroberts@hotmail.com"]
   default_url_options[:host] = "lzacademy.com"
   
   def application_received(regs)
     @regs = regs
-    mail(:to => @regs[0].student.parent.email, :bcc => "info@lzacademy.com, magistraroberts@hotmail.com",
-              :subject => "Your LinguaZone Application")
+    mail(:to => @regs[0].student.parent.email, :subject => "Your LinguaZone Application")
   end
   
   def registration_canceled(reg)
     @reg = reg
-    mail(:to => @reg.student.parent.email, :bcc => "info@lzacademy.com, magistraroberts@hotmail.com",
-              :subject => "[CANCELED] Update on your LinguaZone Application")
+    mail(:to => @reg.student.parent.email, :subject => "[CANCELED] Update on your LinguaZone Application")
   end
 end
