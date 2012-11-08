e318910e9f8520b3356d17a96351f623339b3032
Colin Angevine <colinangevine@gmail.com>
2012-11-07 21:14:49 -0500

    Sorting courses alphabetically by language on schedule page

--- app/controllers/camp_controller.rb
+++ app/controllers/camp_controller.rb
@@ -3,8 +3,8 @@ class CampController < ApplicationController
   end
 
   def schedule
-    @young_camp_courses = Course.camp(2013).younger.all
-    @old_camp_courses = Course.camp(2013).older.all
+    @young_camp_courses = Course.camp(2013).younger.order("programs.language ASC").all
+    @old_camp_courses = Course.camp(2013).older.order("programs.language ASC").all
   end
   
   def typical_day
